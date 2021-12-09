259ef5ee9616c5c44d33a00b2b009358bafc58c7
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-12-09 16:32:13 +0700

    Add necessary Content-Types for a portfolio site

---
+++ src/api/tech-category/controllers/tech-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  tech-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tech-category.tech-category');
---
+++ src/api/tech-category/services/tech-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tech-category service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tech-category.tech-category');
