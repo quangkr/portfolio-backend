90cdd4d2d20424d88fc0af3d31051c771ad1017b
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-12-09 21:56:40 +0700

    Fix PostgreSQL connection

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,21 @@
-const { parse } = require("pg-connection-string");
+const { parse } = require('pg-connection-string');
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse(env('DATABASE_URL'));
 
   return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false }
-        },
-        options: {
-          ssl: false
-        },
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        }
       },
-    },
+    }
   };
 };
